0c134d91dfd8adaa8a8109f1dc0952e6745d39c0
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-07-03 00:38:08 -0700

    Update main_menu.gd
    Start game loads into main scene

--- Godot Game Project/Menus/main_menu.gd
+++ Godot Game Project/Menus/main_menu.gd
@@ -6,14 +6,14 @@ extends Control
 @onready var  setting = $Menu_Options/Options_Margin/HBoxContainer/VBoxContainer/SETTINGS as Button
 @onready var  exit = $Menu_Options/Options_Margin/HBoxContainer/VBoxContainer/EXIT as Button
 
-@onready var start_level = preload("res://Scenes/room.tscn")
+@onready var start_level = preload("res://Scenes/room.tscn") as PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start.button_down.connect(on_start_pressed)
 	exit.button_down.connect(on_exit_pressed)
 	
 func on_start_pressed() -> void:
-	pass
+	get_tree().change_scene_to_packed(start_level)
 	
 func on_exit_pressed() -> void:
 	get_tree().quit()

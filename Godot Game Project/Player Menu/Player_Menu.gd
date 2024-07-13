49202bf482b23c86c7a52dfee25e5c95514f7c3c
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-07-13 14:46:16 -0700

    Pause menu allows menu visibility toggle between different options pressed -> Player_Menu.tscn

--- Godot Game Project/Player Menu/Player_Menu.gd
+++ Godot Game Project/Player Menu/Player_Menu.gd
@@ -1,4 +1,4 @@
-class_name PlayerMenu
+class_name Player_Menu
 extends Camera2D
 #Camera fixed on the player visible when the user pauses and allows the user to select the menu options they desire
 
@@ -7,12 +7,15 @@ extends Camera2D
 
 @onready var id_btn = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/ID_btn as Button
 @onready var equipment_btn = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Equipment_btn as Button
-@onready var item_button = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Item_button as Button
+@onready var item_btn = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Item_btn as Button
 @onready var ability_btn = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Ability_btn as Button
 @onready var settings = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Settings as Button
 @onready var exit = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Panel/MarginContainer/Player_Menu_Btn_Clstr/Exit as Button
 
+@onready var information_container = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Information_Container
+
 @onready var inventory_container = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Information_Container/Inventory_Container
+@onready var settings_tab_container = $Menu_Margin/Player_Menu_Container/Player_Data_Container/Information_Container/Settings_tab_container
 
 var paused = false
 
@@ -40,10 +43,33 @@ func pause_menu():
 #handles button events
 func handle_connect_signal() -> void:
 	equipment_btn.button_down.connect(on_equipment_button_press)
+	id_btn.button_down.connect(on_id_button_press)
+	item_btn.button_down.connect(on_item_button_press)
+	ability_btn.button_down.connect(on_ability_button_press)
+	settings.button_down.connect(on_settings_button_press)
+	exit.button_down.connect(on_exit_button_press)
 
 #function when equipment button is pressed
 func on_equipment_button_press() -> void:
-	inventory_container.visible = !inventory_container
-
+	on_button_press(information_container)
+	inventory_container.visible = !inventory_container.visible
+	
+func on_id_button_press() -> void:
+	pass
+func on_ability_button_press() -> void:
+	pass
+func on_item_button_press() -> void:
+	pass
+func on_settings_button_press() -> void:
+	on_button_press(information_container)
+	settings_tab_container.visible =!settings_tab_container.visible
+func on_exit_button_press() -> void:
+	pass
+	
+#hides all children of container when button pressed
+func on_button_press(container: Node) -> void:
+	for child in container.get_children():
+		if child is Node:
+			child.visible = false
 
 

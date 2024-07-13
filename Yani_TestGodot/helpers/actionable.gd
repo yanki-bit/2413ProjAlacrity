8adf8e5fe82ce105a0082382ff1c7a08f0a4e94e
yanki-bit <149224933+yanki-bit@users.noreply.github.com>
2024-07-12 19:28:08 -0700

    Pushed updated custom balloon

--- Yani_TestGodot/helpers/actionable.gd
+++ Yani_TestGodot/helpers/actionable.gd
@@ -11,11 +11,19 @@
 
 extends Area2D
 
+#get the customized balloon scene and load it 
+const Balloon = preload("res://dialogue/customBalloon/balloon.tscn")
 
 @export var dialogue_resource: DialogueResource #one export to attach the dialogue file
 @export var dialogue_Start: String = "start" #this var specifies from which title are we starting from
 
 # shows the actionable, can be attached
 func _action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_Start)
+	#this uses old default template
+	#DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_Start)
+	
+	#this code uses the new custom balloon
+	var balloon: Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon) #adds to the tree
+	balloon.start(dialogue_resource, dialogue_Start) # starts dialogue
 

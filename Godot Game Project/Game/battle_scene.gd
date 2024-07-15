13b1fcd0e48337c54700434a31d7273e0eff737b
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-07-15 15:18:19 -0700

    Updated Combat UI

--- Godot Game Project/Game/battle_scene.gd
+++ Godot Game Project/Game/battle_scene.gd
@@ -8,9 +8,7 @@ enum EnemyType { Minion, Mini_Boss, Boss }
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$PlayerOptionsContainer/HBoxContainer/Attack.grab_focus()
-	pass
-	
+	$BattleSceneContainer/PlayerBG/PlayerContainer/PlayerActionsContainer/HBoxContainer/PlayerActionCluster/Attack.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -23,7 +21,7 @@ func add_enemies( mainEnemy, minion, numberOfMinions ):
 	enemy1.initialize_stats_in_combat()
 
 	# place main enemy into container
-	$EnemiesVContainer/EnemiesHContainer/MiddleEnemy/MiddleEnemyControl.add_child(enemy1)
+	$BattleSceneContainer/EnemiesContainer/MiddleEnemy/MiddleEnemyControl.add_child(enemy1)
 	enemy1.show_in_fight()
 		# Add requested number of minions
 	match numberOfMinions:
@@ -31,23 +29,22 @@ func add_enemies( mainEnemy, minion, numberOfMinions ):
 			# Add a minion to the left
 			enemy2 = load(minion[0]).instantiate()
 			enemy2.initialize_stats_in_combat()
-			$EnemiesVContainer/EnemiesHContainer/LeftEnemy/LeftEnemyController.add_child(enemy2)
+			$BattleSceneContainer/EnemiesContainer/MiddleEnemy/LeftEnemyControl.add_child(enemy2)
 			enemy2.show_in_fight()
 			
 			# Add a minion to the right
 			enemy3 = load(minion[1]).instantiate()
 			enemy3.initialize_stats_in_combat()
-			$EnemiesVContainer/EnemiesHContainer/RightEnemy/RightEnemyControl.add_child(enemy3)
+			$BattleSceneContainer/EnemiesContainer/MiddleEnemy/RightEnemyControl.add_child(enemy3)
 			enemy3.show_in_fight()
 			
 		1:
 			# Add a minion to the left
 			enemy2 = load(minion[0]).instantiate()
 			enemy2.initialize_stats_in_combat()
-			$EnemiesVContainer/EnemiesHContainer/LeftEnemy/LeftEnemyController.add_child(enemy2)
+			$BattleSceneContainer/EnemiesContainer/MiddleEnemy/LeftEnemyControl.add_child(enemy2)
 			enemy2.show_in_fight()
-			
-
+		
 
 #test
 func _on_attack_pressed():

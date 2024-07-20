f5109dd530435abbadd3879a27ee86ec477556b8
Watermelon339 <dzwu215@gmail.com>
2024-07-19 17:27:47 -0700

    turn attack stat into min and max atk to conform with new combat system

--- Godot Game Project/Scenes/Resources/stat_sheet.gd
+++ Godot Game Project/Scenes/Resources/stat_sheet.gd
@@ -4,7 +4,8 @@ class_name StatSheet
 
 @export var MAX_HP: int
 @export var CURR_HP: int 
-@export var ATK: int
+@export var MIN_ATK: int
+@export var MAX_ATK: int 
 @export var DEF: int
 @export var SPD: int
 @export var LCK: int

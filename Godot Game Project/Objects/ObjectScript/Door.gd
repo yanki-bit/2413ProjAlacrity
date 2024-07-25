449219fbd3d6fed0ee313d77d5de46f73f3e8303
Jraymart <160708949+Jraymart@users.noreply.github.com>
2024-07-24 17:52:57 -0700

    Classmate Change

--- Godot Game Project/Objects/ObjectScript/Door.gd
+++ Godot Game Project/Objects/ObjectScript/Door.gd
@@ -18,8 +18,6 @@ func _on_body_entered(body):
 	if "Player" in body.name:
 		emit_signal("contact")
 		get_node(NodePath("/root/SceneManager")).transition_to_next_scene(next_scene, spawn_location, spawn_direction)
-		
-
 
 func _on_contact():
 	pass

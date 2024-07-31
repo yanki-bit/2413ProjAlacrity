fd84f992018d6a9a2692f4dd15e8bf750b66bda8
marleya25 <165468564+marleya25@users.noreply.github.com>
2024-07-31 00:20:33 -0700

    items_inventoryglobalscript

---
+++ Godot Game Project/items_Inventory_Global.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+var items
+
+func _ready():
+	_read()
+	
+func _read():
+	items = read_from_JSON("res://test_save/itemsInventory.json")
+	if items:
+		for key in items.keys():
+			items[key]["key"] = key
+		
+func read_from_JSON(path: String) -> Dictionary:
+	var file = FileAccess.open(path, FileAccess.READ)
+	var data ={}
+	
+	if file:
+		var json_data = file.get_as_text()  # Get the file content as a string
+		file.close()
+		
+		var json = JSON.new()  # Create a new JSON instance
+		var parse_result = json.parse(json_data)  # Parse the JSON string
+		
+		if parse_result == OK:  # Check if parsing succeeded
+			data = json.get_data()  # Retrieve the parsed data
+	
+		else:
+			push_error("Failed to parse JSON data: %s" % json.get_error_message())
+			
+	else: 
+		push_error("Failed to open file at path: %s" % path)
+	return data		
+func get_item_by_key(key: String) -> Variant:
+			if items and items.has(key):
+				return items[key].duplicate(true)
+			
+			return null

ae8989ad0c1ad86036b8bf13bb35ef01f39bbb2a
marleya25 <165468564+marleya25@users.noreply.github.com>
2024-07-31 12:31:26 -0700

    update test_abilities.tscn

--- Godot Game Project/test_inventory/test_abilities_Inventory.gd
+++ Godot Game Project/test_inventory/test_abilities_Inventory.gd
@@ -32,27 +32,15 @@ func populate_inventory(items_data):
 		var item = items_data[key]
 		
 		var slot_instance = slot_scene.instantiate()
-		#var texture_rect = slot_instance.get_node("inv1")
+		
 		var label = slot_instance.get_node("Label")
-		# Optionally, set the item texture (if you have icons, otherwise this can be empty)
+	
 	 
 		if item.has("Ability"):
 			label.text = item["Ability"]
 		else:
 			label.text = "Unknown Ability"
-	#		var item_texture = preload(item_data.items)  # Path to the texture
-		#texture_rect.texture = item_texture
-		#var texture_path = item.get("icon", null)
-		#if texture_path:
-		#	var item_texture = load(texture_path)
-		#	texture_rect.texture = item_texture
-		#else:
-		#	texture_rect.texture = placeholder_texture
-		# Set the item name
-		#label.text = item.name
 
-		# Ensure the Label is on top of the TextureRect
-		# This is managed in the scene tree by node hierarchy
 
 		# Add the slot instance to the GridContainer
 		grid_container.add_child(slot_instance, true)

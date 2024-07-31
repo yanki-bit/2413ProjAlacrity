236ebeca4729a546d789534d2f8c1832731d9682
marleya25 <165468564+marleya25@users.noreply.github.com>
2024-07-31 12:22:56 -0700

    abilities populated

--- Godot Game Project/test_inventory/test_abilities_Inventory.gd
+++ Godot Game Project/test_inventory/test_abilities_Inventory.gd
@@ -3,6 +3,7 @@ extends Control
 # Preload the InventorySlot scene
 var slot_scene = preload("res://test_inventory/AbilitiesInventorySlot3.tscn")
 
+
 @onready var gridcontainer = get_node("Background/MarginContainer/VBoxContainer/ScrollContainer/GridContainer")
 
 func _ready():
@@ -24,22 +25,34 @@ func _ready():
 		print("Failed to open file")
 
 func populate_inventory(items_data):
-	var grid_container = $GridContainer 
+	var grid_container = $Background/MarginContainer/VBoxContainer/ScrollContainer/GridContainer # Ensure this matches the actual node name
 
-	# Ensure GridContainer is valid
-	if grid_container:
-		# Iterate through items data and instantiate slots
-		for item in items_data.items:
-			var slot_instance = slot_scene.instantiate()
-			var label = slot_instance.get_node("Label")  # Assuming the Label node is named Label
-			
-			if label:
-				# Set the label text to the item name
-				label.text = item.name
-			
-				# Add the slot instance to the GridContainer
-				grid_container.add_child(slot_instance)
-			else:
-				print("Label node not found in slot instance")
-	else:
-		print("GridContainer node not found")
+#iterate over each item in the dictionary
+	for key in items_data.keys():
+		var item = items_data[key]
+		
+		var slot_instance = slot_scene.instantiate()
+		#var texture_rect = slot_instance.get_node("inv1")
+		var label = slot_instance.get_node("Label")
+		# Optionally, set the item texture (if you have icons, otherwise this can be empty)
+	 
+		if item.has("Ability"):
+			label.text = item["Ability"]
+		else:
+			label.text = "Unknown Ability"
+	#		var item_texture = preload(item_data.items)  # Path to the texture
+		#texture_rect.texture = item_texture
+		#var texture_path = item.get("icon", null)
+		#if texture_path:
+		#	var item_texture = load(texture_path)
+		#	texture_rect.texture = item_texture
+		#else:
+		#	texture_rect.texture = placeholder_texture
+		# Set the item name
+		#label.text = item.name
+
+		# Ensure the Label is on top of the TextureRect
+		# This is managed in the scene tree by node hierarchy
+
+		# Add the slot instance to the GridContainer
+		grid_container.add_child(slot_instance, true)
